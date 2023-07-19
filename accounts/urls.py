d1b2b11d3f09b117990ea02deff5d9a8562d81b9
Malachai Onwona <130776588+MalachaiOnwona@users.noreply.github.com>
2023-07-19 11:34:20 -0400

    Delete accounts directory

--- accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from . import views
-
-app_name = 'accounts'
-urlpatterns = [
-    path('', include('django.contrib.auth.urls')),
-    #Registration Page
-    path('register/', views.register, name='register'),
-]
